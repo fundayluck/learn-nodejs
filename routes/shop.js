899ed0eabb55891d65b2a891ea66781b38f280fd
jember08 <afandayul@gmail.com>
2022-04-30 10:14:34 +0700

    finish creating views

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
+const path = require("path");
+
 const express = require("express");
+
+const navigatePath = require("../util/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(path.join(navigatePath, "views", "shop.html"));
 });
 
 module.exports = router;
